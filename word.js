a62bbb32dbe8ee3224cba50e4c58d7853cd4c51e
MuhammetD <doktor.oglu333@gmail.com>
2019-05-18 21:48:00 -0500

    word

--- word.js
+++ word.js
@@ -1,3 +1,22 @@
 //require the letter.js file 
 let Letter = require("./letter");
 
+//word constructor
+
+let Word = function(word){
+    // each word creates an array of letter objects for each letter in the word
+    this.lettersArr = [];
+
+    for(let i = 0; i < word.length; i++){
+        if(word.chaAt(i)=== " "){
+            this.lettersArr.push(" ");
+        }
+        else{
+            this.lettersArr.push(new Letter(word.chaAt(i)));
+        };
+    };
+
+    this.
+
+
+}
